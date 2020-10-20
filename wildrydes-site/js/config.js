3567ddae5dfffc49d86a8f163fbd6e1351a60a9b
Yifan Wu <94yifanwu@gmail.com>
2020-10-20 00:49:10 -0700

    Add files via upload

---
+++ wildrydes-site/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_E6t8g9ciR', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7hq9l32cal2ff97u9ikcgbkbk8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://35hnhpriul.execute-api.us-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
